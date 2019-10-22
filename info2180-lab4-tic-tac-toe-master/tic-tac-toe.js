6373052228e2f5745df885b99d9830fd4dcbfc8a
Jeante <jeanteboreland@gmail.com>
2019-10-22 00:40:20 -0500

    Exercise_2

--- info2180-lab4-tic-tac-toe-master/tic-tac-toe.js
+++ info2180-lab4-tic-tac-toe-master/tic-tac-toe.js
@@ -1,8 +1,45 @@
+
 window.onload=page;
 
 function page(){
-let a = document.getElementById("board").children;
+a = document.getElementById("board").children;
 for (i=0; i<9;i++){
-  a[i].setAttribute("class","square");
+a[i].setAttribute("class","square");
+ setXorO();
+
 }
+}
+
+
+
+let W = [];
+
+
+function setXorO() {
+    let store = document.querySelectorAll("#board > div");
+    for (let i = 0; i < 9; i++) {
+        store[i].onclick = function() {
+            if(W.length === 0 && store[i].innerHTML === "") {
+                store[i].classList.add("square", "X");
+                store[i].innerHTML = "X";
+                W.push("X");
+
+            }
+            else {
+                if (W[W.length - 1] === "X" && store[i].innerHTML === ""){
+                    store[i].classList.add("square", "O");
+                    store[i].innerHTML = "O";
+                    W.push("O");
+
+                }
+                else if (W[W.length - 1] === "O" && store[i].innerHTML === ""){
+                    store[i].classList.add("square", "X");
+                    store[i].innerHTML = "X";
+                    W.push("X");
+
+
+                }
+            }
+        }
+    }
 }
